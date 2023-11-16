3199260ae08495b454f480212731463a42366615
Asliddin Bozorov <asliddiniut@gmail.com>
2023-11-16 15:23:21 +0500

    collection page setup

--- src/components/SubmitBtn.jsx
+++ src/components/SubmitBtn.jsx
@@ -3,6 +3,7 @@ import { useNavigation } from "react-router-dom";
 const SubmitBtn = ({ text }) => {
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "submitting";
+
   return (
     <button
       type="submit"
